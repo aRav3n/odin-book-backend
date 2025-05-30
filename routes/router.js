33a6aec60911138f4ee2c75eb7d1cb1879e2389d
aRav3n <24660772+aRav3n@users.noreply.github.com>
2025-05-30 13:28:46 -0300

    build: set up initial structure of router

--- routes/router.js
+++ routes/router.js
@@ -2,17 +2,38 @@ const { Router } = require("express");
 const controller = require("../controllers/controller");
 const router = Router();
 
-router.get("/friend/:userId/", controller.listFriends);
-router.post("/friend/:userId/add", controller.createFriend);
+router.post("/user");
+router.get("/user/:userId");
+router.put("/user/:userId");
+router.delete("/user/:userId");
+router.post("/user/login");
 
-router.post("/user/login", controller.listUser);
-router.post("/user/signup", controller.createUser);
-router.delete("/user/:userId/delete", controller.deleteUser);
+router.post("/profile");
+router.get("/profile/:profileId");
+router.put("/profile/:profileId");
+router.delete("/profile/:profileId");
 
-router.get("/message/:userId/:messageId", controller.listSingleMessage);
-router.post("/message/:userId/thread/:threadId", controller.createMessage);
+router.post("/post");
+router.get("/post/:postId");
+router.put("/post/:postId");
+router.delete("/post/:postId");
 
-router.get("/thread/:userId", controller.listThreads);
+router.get("/comment/post/:postId");
+router.post("/comment/post/:postId");
+router.get("/comment/reply/:commentId");
+router.post("/comment/reply/:commentId");
+router.put("/comment/self/:commentId");
+router.delete("/comment/self/:commentId");
+
+router.post("/follow");
+router.get("/follow/profile/followers/:profileId");
+router.get("/follow/profile/following/:profileId");
+router.put("/follow/:followId");
+router.delete("/follow/:followId");
+
+router.post("/like/comment/:commentId");
+router.post("/like/post/:postId");
+router.delete("/like/:likeId");
 
 router.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
